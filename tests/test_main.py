1b29d2e7040320e6703882922166b1e158e2db3a
bartoszpudlo <bartoszpudlo07@gmail.com>
2025-11-16 22:01:14 +0100

    checking the test case

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,7 @@ def test_add_function():
     assert add(2, 3) == 5
     assert add(0, 0) == 0
     assert add(5, 5) == 10
+    assert add(0,10) == 2
 
 def test_subtarct_function():
     assert subtract(5, 3) == 2
